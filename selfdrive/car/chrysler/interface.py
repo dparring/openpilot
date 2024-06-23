5ab429647a567475cf30154e02ff4a2b3df6a839
dparring <github@parrington.net>
2024-06-22 19:20:56 -0500

    thicc

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -53,8 +53,8 @@ class CarInterface(CarInterfaceBase):
 
     # Ram
     elif candidate == CAR.RAM_1500_5TH_GEN:
-      ret.steerActuatorDelay = 0.2
-      ret.wheelbase = 3.88
+      ret.steerActuatorDelay = 0.12
+      ret.wheelbase = 3.68
       ret.minSteerSpeed = 0.5
       ret.minEnableSpeed = 14.5
       # Older EPS FW allow steer to zero
